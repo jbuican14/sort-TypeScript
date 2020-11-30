65ce8c907e876e60ba3740f3f88ad52c49bf0db0
Bew <Bew@sysops-MBP.broadband>
2020-11-30 16:01:43 +0000

    add charactersCollection class for sorting character

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,13 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
+import { CharactersCollection } from './CharactersCollection';
 
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
+
+const characterCollection = new CharactersCollection('Xaay');
+const sorter1 = new Sorter(characterCollection);
+sorter1.sort();
+console.log(characterCollection.data);
