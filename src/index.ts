d89ce4d94a606bdea4c018c15c2c5d46d63939f4
Bew <Bew@sysops-MBP.broadband>
2020-12-01 15:48:51 +0000

    Refactor the code to use abstract class instead

--- src/index.ts
+++ src/index.ts
@@ -4,18 +4,17 @@ import { CharactersCollection } from './CharactersCollection';
 import { LinkedList } from './LinkedList';
 
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-console.log('Print Number List of [10, 3, -5, 0] ');
+console.log('Print Number List of [10, 3, -5, 0]');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
-const sorter = new Sorter(numbersCollection);
-sorter.sort();
+numbersCollection.sort();
 console.log(numbersCollection.data);
+
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('Print Character Sort of Xaay');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 const characterCollection = new CharactersCollection('Xaay');
-const sorter1 = new Sorter(characterCollection);
-sorter1.sort();
+characterCollection.sort();
 console.log(characterCollection.data);
 
 const linkedList = new LinkedList();
@@ -27,6 +26,5 @@ linkedList.add(4);
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 console.log('Print Linked List of 500, -10, -3, 4 ');
 console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
-const sorter2 = new Sorter(linkedList);
-sorter2.sort();
+linkedList.sort();
 linkedList.print();
