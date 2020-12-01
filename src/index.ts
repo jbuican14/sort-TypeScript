36c9b7f7235248dd63c758af7eabe74213f6a7ba
Bew <Bew@sysops-MBP.broadband>
2020-12-01 15:18:53 +0000

    Adding LinkedList Class to meet with criteria for interface sort

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('Print Number List of [10, 3, -5, 0] ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
 const sorter = new Sorter(numbersCollection);
 sorter.sort();
 console.log(numbersCollection.data);
-
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('Print Character Sort of Xaay');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
 const characterCollection = new CharactersCollection('Xaay');
 const sorter1 = new Sorter(characterCollection);
 sorter1.sort();
 console.log(characterCollection.data);
+
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('Print Linked List of 500, -10, -3, 4 ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+const sorter2 = new Sorter(linkedList);
+sorter2.sort();
+linkedList.print();
